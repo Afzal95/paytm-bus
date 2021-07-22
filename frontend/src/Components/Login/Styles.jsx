4f14b0d0b4a6d83a4c6af79ec9a1a62fec9bdc3c
Ajmal KV <reallyaju@gmail.com>
2021-07-22 20:23:45 +0530

    login form styles

---
+++ frontend/src/Components/Login/Styles.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const BenefitsCont = styled.div`
+  width: 50%;
+  background-color: #f3f7f8;
+  padding: 25px;
+`;
+
+export const LoginCont = styled.div`
+  width: 50%;
+  margin-top: 15%;
+  padding: 25px;
+  justify-content: center;
+`;
+
+export const HeaderText = styled.h1`
+  color: #333333;
+  font-size: 16px;
+`;
+
+export const BenefitText = styled.p`
+  color: #333333;
+  font-size: 14px;
+  margin-left: 20px;
+`;
+
+export const BenefitItem = styled.div`
+  display: flex;
+`;
+
+export const BlueBox = styled.div`
+  position: absolute;
+  left: 0px;
+  bottom: 0px;
+  height: 0px;
+  width: 100%;
+  border-bottom: 16px solid rgb(0, 185, 245);
+`;
+
+export const ContainerBox = styled.div`
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  border-bottom: 8px solid #012b72;
+`;
