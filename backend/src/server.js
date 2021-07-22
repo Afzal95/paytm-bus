c51670eccbf444013f8242c3259bcb7ecb04d1e8
Ajmal KV <reallyaju@gmail.com>
2021-07-22 10:18:12 +0530

    auth

---
+++ backend/src/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const passport = require("./config/passport");
+const connect = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    done(null, "user");
+});
+passport.deserializeUser(function (id, done) {
+    done(null, "user");
+});
+
+app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: [
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email",
+        ],
+    }),
+);
+
+app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        const { user, token } = req.user;
+        res.status(200).json({
+            user: user,
+            token: token,
+            message: "you are authenticated",
+        });
+    },
+);
+
+const start = async () => {
+	await connect();
+	app.listen(7001, () => {
+		console.log("Listeing on port 7001");
+	});
+};
+
+module.exports = start;
