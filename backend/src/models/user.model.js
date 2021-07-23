bd8b27c941c6d14cae14b3b97209bb8de35bd468
Ajmal KV <reallyaju@gmail.com>
2021-07-23 05:34:17 +0530

    added name

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,8 +3,7 @@ const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
     {
-        first_name: { type: String, required: false },
-        last_name: { type: String, required: false },
+        name: { type: String, required: true },
         email: { type: String, required: true },
         password: { type: String, required: true, minLength: 8 },
     },
