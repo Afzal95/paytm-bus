301cc1009f8edcc14b67bbc9bb086b9ae294f10a
Ajmal KV <reallyaju@gmail.com>
2021-07-24 14:03:02 +0530

    confirm bus page

--- frontend/src/Components/ConfirmBus/FareDetails/FareDetails.jsx
+++ frontend/src/Components/ConfirmBus/FareDetails/FareDetails.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FareBox = styled.div`
+	height: 750px;
+	width: 100%;
+	border: 1px solid rgb(206, 202, 202);
+`;
 
 export const FareDetails=()=>{
     return(
-        <div>
-            Fare Details
-        </div>
+        <FareBox>
+            <p>Fare Details</p>
+			<p>ONWARD FARE</p>
+			<p>Base Fare (3 Travellers)₹3852.00</p>
+			<p>Instant Discount (-)₹150.00</p>
+			<p>Operator GST ₹184.00</p>
+			<div>
+				<p>Total₹3886.00</p>
+			</div>
+			<div>
+				<p>Promocode</p>
+				<input type="text" />
+				<button>Apply</button>
+			</div>
+        </FareBox>
     )
 }
--- frontend/src/Components/ConfirmBus/TravellerInformation/TravellerInformation.jsx
+++ frontend/src/Components/ConfirmBus/TravellerInformation/TravellerInformation.jsx
@@ -1,9 +1,117 @@
 import React from 'react';
-
+import styled from 'styled-components';
+const TravelInfoCont = styled.div`
+	width: 100%;
+	margin-top: 3%;
+	border: 1px solid rgb(206, 202, 202);
+	box-shadow: rgba(32, 33, 36, 0.18) 0px 1px 6px 0px;
+	overflow: auto;
+`;
+const Checkbox = styled.input`
+	height: 15px;
+	width: 15px;
+`;
+const Para = styled.p`
+	font-size: 15px;
+	margin-left: 3%;
+	font-family: "" Open Sans ", Arial, Helvetica, sans-serif";
+	color: rgb(80, 80, 80);
+	font-weight: 600;
+	text-size-adjust: 100%;
+`;
+const Logindetails = styled.div`
+	height: 70px;
+	width: 95%;
+	display: flex;
+	margin-left: 3%;
+	margin-top: 1%;
+	/* display: flex; */
+	/* flex-direction: column; */
+	/* border: 1px solid rgb(206, 202, 202); */
+`;
+const Input = styled.input`
+	height: 40px;
+	width: 250px;
+	border: 1px solid rgb(206, 202, 202);
+	margin-left: 3%;
+	margin-top: 1%;
+`;
+const Select = styled.select`
+	height: 40px;
+	width: 250px;
+	border: 1px solid rgb(206, 202, 202);
+	margin-left: 3%;
+	margin-top: 1%;
+	text-size-adjust: 100%;
+`;
+const ReviewTitleCont = styled.div`
+height: 45px;
+background-color: #e9edee;
+padding: 1px;
+`;
+const ReviewTitle = styled.div`
+color:#4a4a4a;
+font-size: 16px;
+font-weight: 600;
+text-align: left;
+margin-top: 10px;
+margin-left: 10px;
+`;
 export const TravellerInformation=()=>{
+	const passangers = [{id:1,seat:'A1'},{id:2,seat:'A2'}];
     return(
-        <div>
-            Traveller Information
-        </div>
+        <TravelInfoCont>
+            <ReviewTitleCont>
+                <ReviewTitle>02 Traveller Information</ReviewTitle>
+            </ReviewTitleCont>
+			{
+				passangers.map(item=>
+					<Logindetails key={item.id} >
+						{/* <div style={{border:"1px solid black"}}> */}
+							
+							<p>Travller {item.id}</p>
+							<Select name="gender">
+								<option value="male">Male</option>
+								<option value="female">Female</option>
+							</Select>
+							<p>Seat {item.seat}</p>
+						{/* </div> */}
+						<br />
+						{/* <div>aaaaa</div> */}
+						<div style={{display:"flex",border:"1px solid black"}}>
+							<Input placeholder="Full Name"></Input>
+							<Input placeholder="Age"></Input>
+					
+							
+						</div>
+					
+				</Logindetails>
+					)
+			}
+				
+				<Para>
+				Contact Information
+
+				</Para>
+				<p>Your ticket and PNR Info will be sent to these.</p>
+				<Logindetails>
+					<Input placeholder="Email ID"></Input>
+					<Input placeholder="Mobile No."></Input>
+				</Logindetails>
+			<br />
+			
+			<br />
+				<Para>
+					<Checkbox type="checkbox"></Checkbox>
+					I agree to all the{" "}
+					<a
+						href="https://www.abhibus.com/insterms/TATA_AIG"
+						style={{ color: "#00b9f5", textDecoration: "none" }}
+					>
+						terms and conditions
+					</a>
+				</Para>
+			<br />
+        </TravelInfoCont>
     )
 }
