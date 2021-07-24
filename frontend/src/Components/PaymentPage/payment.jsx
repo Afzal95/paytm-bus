a8fb3693a8b2b543fd12da14c30820b9b9c05a63
afzal <afzalpolite@gmail.com>
2021-07-25 03:24:22 +0530

    payment page done

--- frontend/src/Components/PaymentPage/payment.jsx
+++ frontend/src/Components/PaymentPage/payment.jsx
@@ -1,9 +1,34 @@
 // import {Link} from "react-router-dom";
+import { useState } from "react";
 import styles from "./Payment.module.css";
+import { TextField } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 function Payment(){
 
-    return <div>
+    const [credit,setCredit] = useState(false);
+    const [debit,setDebit] = useState(false);
+    const [UPI,setUpi] = useState(false);
+    const [UpiId,setUpiId] = useState(true);
+
+    const handle =(e)=>{
+        const val = e.target.value;
+        if(val==="credit"){
+            setCredit(true);
+            setDebit(false);
+            setUpi(false);
+        }else if(val==="debit"){
+            setCredit(false);
+            setDebit(true);
+            setUpi(false);
+        }else if(val==="upi"){
+            setCredit(false);
+            setDebit(false);
+            setUpi(true);
+        }
+    }
+
+    return <div style={{paddingBottom:"180px"}}>
     <div className={styles.iconDiv}>
     <img src="https://staticpg.paytm.in/pgp/web/assets/logo.png" width="100%" height="50%" alt="icon" />
     </div>
@@ -24,23 +49,52 @@ function Payment(){
         </div>
         </div>
     </div>
-    <div className={styles.firstDiv}>
+    <div className={styles.firstDiv} style={{marginTop:"30px",paddingTop:"30px",borderRadius:"0"}}>
         <div className={styles.goBack}>
-            {/* <Link>Go back</Link> */}
-            Go back
+            SELECT AN OPTION TO PAY
         </div>
-        <hr style={{borderBottom:".4px light grey"}} />
         <div style={{display:"flex",marginLeft:"20px",justifyContent:"space-between",marginRight:"20px"}}>
-        <div>
-        <h3 style={{fontSize:"18px",marginBottom:"-10px"}}>Paytm Bus tickets Order</h3>
-        <p>Transaction ID: 14461199498</p>
+        <div style={{display:"flex",justifyContent:"space-between",width:"160px"}}>
+        <div style={{width:"20px",height:"20px",marginTop:"-4px"}}><input disabled type="checkbox" style={{marginTop:"20px",fontSize:"20px",width:"100%",height:"100%"}} /></div>
+        <p>Paytm Balance<br/><span style={{marginLeft:"5px",fontSize:"13px"}}>Available balance 0</span><br/></p>
         </div>
-        <div>
-        <p style={{marginBottom:"-10px"}}>Amount to be paid</p>
-        <h3>Rs799</h3>
-        </div>
+        {/* {show && <div>
+        <button style={{width:"190px",border:"none",cursor:"pointer",height:"50px",padding:"10px",color:"white",backgroundColor:"#34c9c9"}}>PAY 799Rs</button>
+        </div>} */}
         </div>
     </div>
+    <div className={styles.firstDiv} style={{paddingTop:"10px",borderRadius:"0"}}>
+        {/* <div className={styles.goBack}>
+            SELECT AN OPTION TO PAY
+        </div> */}
+        <div style={{width:"160px",marginTop:"-40px"}}>
+                <input style={{height:"20px",width:"20px",marginLeft:"20px"}} type="radio" value="credit" name="method" onChange={handle} /> <span style={{marginLeft:"20px"}}>&nbsp;</span> Credit Card
+        </div>
+        {credit && <div style={{display:"flex",marginTop:"20px",marginLeft:"80px"}}>
+        <TextField style={{width:"300px"}} id="outlined-basic" label="card no" type="number" variant="outlined" />
+        <TextField style={{width:"140px",marginLeft:"20px"}} id="outlined-basic" label="card expiry date" type="number" placeholder="MM/YY" variant="outlined" />
+        <TextField style={{width:"140px",marginLeft:"20px"}} id="outlined-basic" label="CVV" type="number" placeholder="CVV" variant="outlined" />
+        <Button variant="contained" size="large" style={{backgroundColor:"#34c9c9",width:"200px",marginLeft:"20px"}} >Pay 799</Button>
+        </div>}
+        <div style={{width:"160px",marginTop:"50px"}}>
+                <input style={{height:"20px",width:"20px",marginLeft:"15px"}} type="radio" value="debit" name="method" onChange={handle} /> <span style={{marginLeft:"20px"}}>&nbsp;</span> Debit Card
+        </div>
+        {debit && <div style={{display:"flex",marginTop:"20px",marginLeft:"80px"}}>
+        <TextField style={{width:"300px"}} id="outlined-basic" label="card no" type="number" variant="outlined" />
+        <TextField style={{width:"140px",marginLeft:"20px"}} id="outlined-basic" label="card expiry date" type="number" placeholder="MM/YY" variant="outlined" />
+        <TextField style={{width:"140px",marginLeft:"20px"}} id="outlined-basic" label="CVV" type="number" placeholder="CVV" variant="outlined" />
+        <Button variant="contained" size="large" style={{backgroundColor:"#34c9c9",width:"200px",marginLeft:"20px"}} >Pay 799</Button>
+        </div>}
+        <div style={{width:"160px",marginTop:"50px",marginLeft:"-20px"}}>
+                <input style={{height:"20px",width:"20px"}} type="radio" value="upi" name="method" onChange={handle} /> <span style={{marginLeft:"30px"}}></span> UPI
+        </div>
+        {UPI && <div style={{display:"flex",marginTop:"20px",marginLeft:"80px",paddingBottom:"50px",backgroundColor:"#f3f7f8"}}>
+        <TextField onChange={e=>setUpiId(false)} style={{width:"300px"}} id="outlined-basic" label="UPI Id" placeholder="Enter UPI Id" type="text" variant="outlined" />
+        <TextField disabled={UpiId} style={{width:"140px",marginLeft:"20px"}} id="outlined-basic" label="OTP" type="number" placeholder="enter otp" variant="outlined" />
+        <Button variant="contained" size="large" style={{backgroundColor:"#34c9c9",width:"200px",marginLeft:"20px"}} >Pay 799</Button>
+        </div> }
+    </div>
+    
     </div>
 }
 
