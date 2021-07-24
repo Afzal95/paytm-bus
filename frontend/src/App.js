fae3e9bcb7b0f73eec6e2bd56d34c5af0b8a0b39
sy3dyaseen <sy3dyaseen@gmail.com>
2021-07-24 19:02:39 +0530

    BusBookingAndFilters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { Homepage } from "./Components/Homepage/Homepage.jsx";
 import { Faq } from "./Components/Faq/Faq.jsx";
 import { Payment } from "./Components/PaymentPage/payment.jsx";
+import { BusBookingAndFilters } from "./Components/BusBooking&Filters/BusBookingAndFilters.jsx";
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar/>
       <Homepage/> */}
-      <Payment />
+      {/* <Payment /> */}
+      <BusBookingAndFilters />
      
     </div>
   );
